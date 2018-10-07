384fec8fb5fa4257733c7d12f602928fca1b5d09
Dmitrii Sutiagin <f3flight@gmail.com>
2018-10-07 15:41:41 -0700

    Initial cleanup attempt

---
+++ appointments/gcal.py
@@ -0,0 +1,173 @@
+from google.oauth2.service_account import Credentials as gcreds
+from googleapiclient.discovery import build as gbuild
+
+import copy
+import datetime
+import pytz
+import re
+
+
+class AppointmentManager(object):
+    """Uses Google service account to create appointments w. Calendar API
+
+       Attributes:
+           tag_field (str): defines which field in Google calendar structure
+               is used to store calendar's type and schedule name. The content
+               of this field should not be modified by user.
+           slots_tag (str): tag to indicate slot calendar.
+           appointments_tag (str): tag to indicate appointments calendar.
+           schedule_name (str): name of appointment schedule.
+               Used as part of summary for generated calendars and events.
+           timezone (str): schedule's timezone i.e. 'America/Los Angeles'.
+           location (str): address of appointment, added into calendar event.
+           admin (str): email of admin user (schedule owner). This user will
+               be set as owner of the generated calendars and will see them
+               in calendar list in Google Calendar.
+           fixed_attendee (str): email to be added to all generated
+               appointments (for example, service provider) to generate
+               notifications about new appointments
+           event_description (str): text put as description in generated
+               appointments.
+    """
+
+    def __init__(self, creds_file, config=None):
+        """Create AppointmentManager object
+
+        Args:
+            creds_file (str): path to service account's creds json.
+                This file should be generated via Google API credentials page:
+                https://console.developers.google.com/apis/credentials
+            config (dict, optional): config dict, if provided is merged into
+                object's attributes, allows to change any existing attribute.
+
+        """
+
+        # Below are defaults for all attributes. Change by passing config dict.
+        self.tag_field = 'description'  # do not modify unnecessarily
+        self.slots_tag = '#slots'  # do not modify unnecessarily
+        self.appointments_tag = '#appointments'  # do not modify unnecessarily
+        self.schedule_name = 'my services'
+        self.timezone = 'America/Los_Angeles'
+        self.location = '1 Nowhere street, 00000'
+        self.admin = 'admin@admin.com'
+        self.fixed_attendee = 'admin@admin.com'
+        self.event_description = ('Looking forward to meeting you!'
+                                  ' Need to change or cancel? Call/text me at'
+                                  ' +12345678901')
+
+        self.configure(config)
+
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        creds = gcreds.from_service_account_file(creds_file, scopes=SCOPES)
+
+        self._api = gbuild('calendar', 'v3', credentials=creds)
+
+        # the following section finds or creates slot and appointment calendars
+        cals = self._api.calendarList().list().execute()
+        slots_cal = None
+        appointments_cal = None
+        for cal in cals['items']:
+            if cal.get(self.tag_field, '').startswith(self.slots_tag):
+                slots_cal = cal['id']
+            elif cal.get(self.tag_field, '').startswith(self.appointments_tag):
+                appointments_cal = cal['id']
+        if slots_cal:
+            self.slots_cal = slots_cal
+        else:
+            self.slots_cal = self.create_cal(self.slots_tag, 'slots')
+        if appointments_cal:
+            self.appointments_cal = appointments_cal
+        else:
+            self.appointments_cal = self.create_cal(self.appointments_tag,
+                                                    'appointments')
+        self.refresh()
+
+    def refresh(self):
+        iso_now = '%s%s' % (datetime.datetime.now().isoformat('T'),
+                            iso_tz(self.timezone))
+        events = self._api.events()
+        self.slots = events.list(calendarId=self.slots_cal,
+                                 orderBy='startTime',
+                                 singleEvents=True,
+                                 timeMin=iso_now).execute()
+        self.appointments = events.list(calendarId=self.appointments_cal,
+                                        orderBy='startTime',
+                                        singleEvents=True,
+                                        timeMin=iso_now).execute()
+        self.free_slots = self.get_free_slots()
+
+    def configure(self, config_dict):
+        if config_dict:
+            for key, value in config_dict.items():
+                if hasattr(self, key) and not key.startswith('_'):
+                    setattr(self, key, value)
+
+    def get_free_slots(self):
+        free = copy.copy(self.slots['items'])
+        for appointment in self.appointments['items']:
+            i = 0
+            while i < len(free):
+                apnt_start = key_to_time(appointment, 'start')
+                apnt_end = key_to_time(appointment, 'end')
+                slot_start = key_to_time(free[i], 'start')
+                slot_end = key_to_time(free[i], 'end')
+                if apnt_start < slot_end and apnt_end > slot_start:
+                    free.pop(i)
+                    continue
+                i += 1
+        return free
+
+    def create_cal(self, tag, name):
+        body = {'summary': '%s - %s' % (self.schedule_name, name),
+                'timeZone': self.timezone,
+                'description': '%s %s' % (tag, self.schedule_name)}
+        cal = self.api.calendars().insert(body=body).execute()
+        body2 = {'scope': {'type': 'user', 'value': self.admin},
+                 'role': 'owner'}
+        self.api.acl().insert(calendarId=cal['id'], body=body2).execute()
+        return cal['id']
+
+    def create_appointment(self, chosen_slot_id, data):
+        self.refresh()
+        chosen_slot = None
+        for slot in self.free_slots:
+            if slot['id'] == chosen_slot_id:
+                chosen_slot = slot
+        if not chosen_slot:
+            return False
+        name = ' at '.join([data['name'], data['phone']])
+        summary = '%s: %s' % (self.schedule_name, name)
+        event = {'summary': summary,
+                 'location': self.location,
+                 'description': self.event_description,
+                 'start': chosen_slot['start'],
+                 'end': chosen_slot['end']}
+        if getattr(self, 'fixed_attendee'):
+            event['attendees'] = [{'email': self.fixed_attendee}]
+        if data.get('email'):
+            email = {'email': data['email']}
+            if event.get('attendees'):
+                event['attendees'].append(email)
+            else:
+                event['attendees'] = [email]
+        self._api.events().insert(calendarId=self.appointments_cal,
+                                  body=event,
+                                  sendNotifications=True).execute()
+        return True
+
+
+def key_to_time(event, key_name):
+    timestamp = event[key_name]['dateTime']
+    # taken from https://stackoverflow.com/a/25878651/3952027
+    # this regex removes all colons and all
+    # dashes EXCEPT for the dash indicating + or - utc offset for the timezone
+    conformed_timestamp = re.sub(r'[:]|([-](?!((\d{2}[:]\d{2})|(\d{4}))$))',
+                                 '', timestamp)
+    return datetime.datetime.strptime(conformed_timestamp, "%Y%m%dT%H%M%S%z")
+
+
+def iso_tz(timezone_str):
+    tz_now = datetime.datetime.now(pytz.timezone(timezone_str)).strftime('%z')
+    # if ':' not in tz_now:
+    #     tz_now =  '%s:%s' % (tz_now[:3], tz_now[3:])
+    return tz_now
---
+++ example_http_frontend.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+
+import html
+import ssl
+import yaml
+import os
+
+from appointments import gcal
+
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    cfg = {} if not os.path.exists('config.yaml') else yaml.load(open('config.yaml', 'r'))
+    cal = gcal.AppointmentManager('credentials.json', cfg)
+
+    # GET
+    def do_GET(self):
+        date_format = '%B %d, %-I:%M%p'
+        self.cal.refresh()
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = 'Please pick available slot:<br /><form action="/" method="post">'
+        if not self.cal.free_slots:
+            message = 'Sorry, no slots are available for booking at this time! Try again later!'
+        else:
+            for slot in self.cal.free_slots:
+                message += '<input type="radio" name="slot" value="%s" />%s<br>' % (slot['id'], gcal.key_to_time(slot, 'start').strftime(date_format))
+            message += '<label for="name">Name:</label><input type="text" placeholder="John Doe" name="name" required/><br />'
+            message += '<label for="phone">Phone:</label><input type="text" placeholder="+14081234567" name="phone" required/><br />'
+            message += '<label for="email">Email:</label><input type="email" placeholder="abc@gmail.com" name="email"/><br />'
+            message += '<input type="submit" value="book" />'
+        # message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+    def do_POST(self):
+        failed = False
+        msg = 'Appointment booked! Thank you!<br /><a href="/">Book some more...</a>'
+        fail_msg = 'Sorry, this slot is already booked! Better luck next time!<br /><a href="/">Try again...</a>'
+        length = int(self.headers['content-length'])
+        bytedict = parse_qs(self.rfile.read(length))
+        # bytedict structure: {b'key': [b'value'], ...}; not sure why so
+        #with open('debug.log', 'a') as f:
+        #    f.write('\n---POST:\n')
+        #    f.write(yaml.dump(bytedict))
+        postvars = dict((k.decode("utf-8"), html.unescape(v[0].decode("utf-8"))) for k, v in bytedict.items())
+        if postvars.get('slot'):
+            try:
+                if not self.cal.create_appointment(postvars['slot'], postvars):
+                    failed = True
+            except:
+                failed = True
+                if postvars['email']:
+                    fail_msg = 'Something went wrong! Please check if your email is valid...<br /><a href="/">Try again...</a>'
+                else:
+                    fail_msg = 'Something went wrong! Please let us know about it and we will try to fix it asap.<br /><a href="/">Try again...</a>'
+        else:
+            failed = True
+            fail_msg = 'Looks like you forgot to select a time slot?<br /><a href="/">Try again...</a>'
+
+        # Send response status code
+        self.send_response(500 if failed else 200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes(str(fail_msg if failed else msg), "utf8"))
+
+
+def run():
+    print('starting server...')
+
+    # Server settings
+    settings = {'address': '127.0.0.1',
+                'port': 80,
+                'ssl': False,
+                'keyfile': None,
+                'certfile': None}
+    if os.path.exists('http_config.yaml'):
+        with open('http_config.yaml', 'r') as f:
+            settings.update(yaml.load(f))
+    server_address = (settings['address'], settings['port'])
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    if settings['ssl']:
+        httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True,
+                                       keyfile=settings['keyfile'],
+                                       certfile=settings['certfile'])
+    print('running server...')
+    httpd.serve_forever()
+
+
+run()
